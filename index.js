3068829382478765ba5b05ff507390839fb8f29c
ghyarhmoussa <ghyathmoussa@hotmail.com>
2020-09-24 23:29:43 +0300

    adding function to keep control on statusBar

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './src/views/App';
 import {name as appName} from './app.json';
+import phoneH from './src/helps/phoneH';
+
+if (phoneH.android) UIManager.setLayoutAnimationEnabledExperimental(true);
+
+const codepushAYARLAR = {
+    checkFrequency: codePush.CheckFrequency.MANUAL, //güncelleme kontrol sıklığı
+};
 
 AppRegistry.registerComponent(appName, () => App);
