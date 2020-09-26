d558c7ba10e7335498605270b6870389caa341a7
ghyarhmoussa <ghyathmoussa@hotmail.com>
2020-09-26 14:12:10 +0300

    edthing index.js and app's images

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-/**
- * @format
- */
-
 import {AppRegistry,UIManager} from 'react-native';
+// import 'react-native-console-time-polyfill';
 import App from './src/views/App';
 import {name as appName} from './app.json';
 import phoneH from './src/helps/phoneH';
